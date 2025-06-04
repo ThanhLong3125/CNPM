bc016ed49ee9d392bc916c61ef5b1fd6fcfab092
Phuong Thao <dnpthao3103@gmail.com>
2025-06-04 13:06:42 +0700

    setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+import LoginPage from './components/Page/LoginPage'
+import HomePage from './components/Page/HomePage'
+import DoctorMain from './components/Doctor/DoctorMain'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <DoctorMain/>
+   
+  )
+}
+export default App
+
