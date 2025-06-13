7114ec3f8e87efd1b3a46c37f52812c65577e652
Pham Hoang Thien <phamthien27113211@gmail.com>
2025-06-13 21:59:20 +0700

    Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
+
+import Navbar from './components/layout/Navbar'
 import LoginPage from './components/Page/LoginPage'
 import HomePage from './components/Page/HomePage'
 import DoctorMain from './components/Doctor/DoctorMain'
 import PatientAwaitDetail from './components/Doctor/PatientAwaitDetail'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './components/layout/Navbar'
 import History_records from "./components/Staff/History_records"
 import ReceptionProfile from "./components/Staff/ReceptionProfile"
 const App = () => {
   return (
     <BrowserRouter>
+
       <Navbar />
+
       <Routes>
-        {/* 
-        <Route path='/detail/:patient_id' element={< PatientAwaitDetail/>}/> 
-        < Route path='/' element={<History_records />} />
-         < Route path='/' element={<ReceptionProfile />} />*/}
-        < Route path='/' element={<DoctorMain />} />
+        {
+          /* 
+          < Route path='/detail/:patient_id' element={< PatientAwaitDetail/>}   /> 
+          < Route path='/' element={<History_records />}                        />
+          < Route path='/' element={<DoctorMain />}                             />
+          < Route path='/' element={<ReceptionProfile />}                       />
+          */
+        }
+
 
       </Routes>
     </BrowserRouter>
