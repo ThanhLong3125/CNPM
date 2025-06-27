804dc6131b03779de8e6622dd8fa64f072b39c27
Pham Hoang Thien <phamthien27113211@gmail.com>
2025-06-27 14:36:35 +0700

    a

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import DoctorMain from './components/Doctor/DoctorMain'
 import PatientAwaitDetail from './components/Doctor/PatientAwaitDetail'
 import History_records from "./components/Staff/History_records"
 import ReceptionProfile from "./components/Staff/ReceptionProfile"
+import MedicalRecord from './components/Doctor/MedicalRecord';
 
 const App = () => {
   return (
@@ -17,15 +18,17 @@ const App = () => {
       <Routes>
 
         {/* 
-      
+      < route path='/MedicalRecord/:patient_id' element {< MedicalRecord />} />
       < Route path='/History_records' element={<History_records />} />
       < Route path='/detail/:patient_id' element={< PatientAwaitDetail />} />
       < Route path='/' element={<DoctorMain />} />
       < Route path='/' element={<ReceptionProfile />} />
-         
+      < Route path='/' element={<ReceptionProfile />} />
+      < Route path='/HomePage' element={<HomePage />} />
+      
        */}
-        < Route path='/' element={<ReceptionProfile />} />
-        < Route path='/HomePage' element={<HomePage />} />
+        < Route path='/doctor' element={<DoctorMain />} />
+        < Route path='/MedicalRecord/:patient_id' element={< MedicalRecord />} />
 
       </Routes>
     </BrowserRouter>
